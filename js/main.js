d72853b608b4a7c7a91b2d0bfa090c8ff32b0d99
Sergio <sergiolpz.2002@gmail.com>
2026-04-05 22:32:46 +0200

    añadido popup detalle

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { token } from "./config.js";
-
+const imagenURL = "https://image.tmdb.org/t/p/w500";
 async function datosPeliculas() {
   const options = {
     method: "GET",
@@ -32,7 +32,6 @@ async function pintarPeliculas() {
   pelis.forEach((e) => {
     const div = document.createElement("div");
     div.className = "pelicula";
-    const imagenURL = "https://image.tmdb.org/t/p/w500";
 
     div.innerHTML = `
             <h3> ${e.title} </h3>
@@ -40,6 +39,9 @@ async function pintarPeliculas() {
         `;
 
     cont.appendChild(div);
+    div.addEventListener("click", () => {
+      popup(e);
+    })
   });
 }
 pintarPeliculas();
@@ -47,22 +49,31 @@ pintarPeliculas();
 const inp = document.getElementById("buscador");
 inp.addEventListener("input", function (e) {
   let valor = e.target.value.trim().toLowerCase();
-  // const peliculas = document.getElementsByClassName("pelicula");
   const peliculas = document.querySelectorAll(".pelicula");
-  // console.log(peliculas);
-  // console.log(document.querySelectorAll(".pelicula"));
 
   peliculas.forEach((e) => {
     const titulo = e.querySelector("h3").textContent.toLowerCase();
-    console.log(titulo);
 
     if (titulo.includes(valor)) {
-      e.classList.remove("oculta")
-    }else e.classList.add("oculta")
+      e.classList.remove("oculta");
+    } else e.classList.add("oculta");
 
     console.log("titulo: " + titulo + " valor: " + valor);
   });
-
-
-  
 });
+
+function popup(pelicula) {
+  const existe = document.getElementById("popup")
+  if (existe) {existe.remove()}
+  const cont = document.getElementById("contenedor");
+  const div = document.createElement("div");
+  div.id = "popup"
+  div.className = "popup"
+  div.innerHTML = `<h2> ${pelicula.title} </h2>
+    <img src="${imagenURL}${pelicula.poster_path} alt="imagen de la pelicula" loading="lazy">
+    <p> ${pelicula.backdrop_path}
+
+  `
+  cont.appendChild(div)
+
+}
